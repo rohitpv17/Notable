c9123150a26278124f7e196982f5d99e76026169
rohitpv17 <rohitpv17@gmail.com>
2025-02-10 23:37:11 +0530

    Added Searchbar and Profileinfo in Navbar

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,9 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
+import ProfileInfo from "../Cards/ProfileInfo";
+import { useNavigate } from "react-router-dom";
+import SearchBar from "../SearchBar/SearchBar";
 
 const Navbar = () => {
+  const [searchQuery, setSearchQuery] = useState("");
+  const navigate = useNavigate;
+  const onLogout = () => {
+    navigate("/login");
+  };
+  const handleSearch = () => {};
+  const onClearSearch = () => {
+    setSearchQuery("");
+  };
   return (
     <div className="bg-white flex items-center justify-between px-6 drop-shadow">
-      <h2 className="text-xl font-medium text-black py-2">Notable</h2>
+      <h2 className="text-xl font-medium text-black py-5">Notable</h2>
+      <SearchBar
+        value={searchQuery}
+        onChange={({ target }) => {
+          setSearchQuery(target.value);
+        }}
+        handleSearch={handleSearch}
+        onClearSearch={onClearSearch}
+      />
+      <ProfileInfo onLogout={onLogout} />
     </div>
   );
 };
