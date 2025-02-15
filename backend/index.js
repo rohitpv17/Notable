09a0498e71ca1d96984bcfcd13d124eacdab0043
rohitpv17 <rohitpv17@gmail.com>
2025-02-15 21:13:45 +0530

    Completed Edit Note API

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 const mongoose = require("mongoose");
 mongoose.connect(process.env.CONNECTION_STRING);
 const User = require("./models/user.model");
+const Note = require("./models/note.model");
 
 const express = require("express");
 const cors = require("cors");
@@ -86,6 +87,37 @@ app.post("/login", async (req, res) => {
     return res.status(400).json({
       error: true,
       message: "Invalid Credentials",
+    });
+  }
+});
+app.post("/add-note", authenticateToken, async (req, res) => {
+  const { title, content, tags } = req.body;
+  const { user } = req.user;
+  if (!title) {
+    return res.status(400).json({ error: true, message: "Title is Required." });
+  }
+  if (!content) {
+    return res
+      .status(400)
+      .json({ error: true, message: "Content is Required," });
+  }
+  try {
+    const note = new Note({
+      title,
+      content,
+      tags: tags || [],
+      userId: user._id,
+    });
+    await note.save();
+    return res.json({
+      error: false,
+      note,
+      message: "Note Added Successfully.",
+    });
+  } catch (error) {
+    return res.status(500).json({
+      error: true,
+      message: "Internal Server Error.",
     });
   }
 });
