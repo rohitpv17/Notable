544e98f2a0b053a74cd9b08bd38fbd51b1206be7
rohitpv17 <rohitpv17@gmail.com>
2025-02-15 23:29:48 +0530

    Completed Get All Notes API

--- backend/index.js
+++ backend/index.js
@@ -160,6 +160,22 @@ app.put("/edit-note/:noteId", authenticateToken, async (req, res) => {
     });
   }
 });
+app.get("/get-all-notes/", authenticateToken, async (req, res) => {
+  const { user } = req.user;
+  try {
+    const notes = await Note.find({ userId: user._id }).sort({ isPinned: -1 });
+    return res.json({
+      error: false,
+      notes,
+      message: "Retrieved Successfully.",
+    });
+  } catch (error) {
+    return res.status(500).json({
+      error: true,
+      message: "Internal Server Error.",
+    });
+  }
+});
 app.listen(8000);
 
 module.exports = app;
